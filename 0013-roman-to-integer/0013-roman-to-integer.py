072b6c6b1059fa4c13502f86766f76884a96d621
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-05-16 23:17:16 -0400

    Time: 65 ms (23.59%), Space: 16.3 MB (13.06%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -2,8 +2,10 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         result = 0
+        # iterate in reverse order
         for element in s[ : : -1]:
-            
+            # if current result is 5 or 10 times of current digit, minus the digit
+            # else plus the digit
             if result // d[element] == 5 or result // d[element] == 10:
                 result -= d[element]
             else:

fa4be6f361fe9b8499f0cd6166544347d1cc1e84
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-05-17 23:09:18 -0400

    Time: 545 ms (76.55%), Space: 16.4 MB (35.34%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,22 @@
+class Solution:
+   
+    def expandAroundCenter(self, s, left, right):
+        # check palindrome from a substring s[left:right+1]
+        while left>=0 and right<len(s) and (s[left] == s[right]):
+            left -= 1
+            right += 1
+        return left + 1, right - 1
+    
+    def longestPalindrome(self, s: str) -> str:
+        start, end = 0, 0
+        for i in range(len(s)):
+            # start with two possible minimal palindrome
+            left1, right1 = self.expandAroundCenter(s, i, i)
+            left2, right2 = self.expandAroundCenter(s, i, i+1)
+            # pick the longest string from the two results
+            if right1 -left1 > end - start:
+                start, end = left1, right1
+            if right2 - left2 > end -start:
+                start, end = left2, right2
+            
+        return s[start: end+1]

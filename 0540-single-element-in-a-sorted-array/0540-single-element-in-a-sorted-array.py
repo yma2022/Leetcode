632d1019c30a64ac2307d8dfaa8702001b9aa278
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-05-25 12:15:06 -0400

    Time: 188 ms (22.38%), Space: 26 MB (39.57%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -4,12 +4,10 @@ class Solution:
         right = len(nums) - 1
         while left < right:
             mid = (left + right) // 2
-            if (mid % 2 == 0 and nums[mid] == nums[mid-1]) or (mid % 2 != 0 and nums[mid] == nums[mid+1]):
-                right = mid - 1
-            elif (mid % 2 != 0 and nums[mid] == nums[mid-1]) or (mid % 2 == 0 and nums[mid] == nums[mid+1]):
-                left = mid + 1
+            if mid % 2 != 0:
+                mid -= 1
+            if nums[mid] == nums[mid + 1]:
+                left = mid + 2
             else:
-                return nums[mid]
-                
-                
+                right = mid                
         return nums[left]

4c8829f805fdbc72c828a83ee18e5f7bc71d30fa
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-05-30 23:06:28 -0400

    Time: 55 ms (7.72%), Space: 16.3 MB (26.69%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,21 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        columns = len(matrix[0])
+        rows = len(matrix)
+        
+        visited = [[False] * columns for _ in range(rows)]
+        total = rows * columns 
+        order = [0] * total
+        
+        directions = [[0,1], [1,0], [0,-1],[-1,0]]
+        row, column = 0, 0
+        directionIndex = 0
+        for i in range(total):
+            order[i] = matrix[row][column]
+            visited[row][column] = True
+            nextRow, nextColumn = row + directions[directionIndex][0], column + directions[directionIndex][1]
+            if not (0 <= nextRow < rows and 0 <= nextColumn < columns and not visited[nextRow][nextColumn]):
+                directionIndex = (directionIndex + 1) % 4
+            row += directions[directionIndex][0]
+            column += directions[directionIndex][1]
+        return order

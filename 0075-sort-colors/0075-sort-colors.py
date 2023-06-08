fa45b96d143f91e66df751ff75210f11909b68cc
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-06-08 14:48:57 -0400

    Time: 49 ms (44.60%), Space: 16.4 MB (17.30%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -8,10 +8,11 @@ class Solution:
         p1 = n - 1
         curr = 0
         while curr <= p1:
-            if nums[curr] == 0:
+            if curr < p0:
+                curr += 1
+            elif nums[curr] == 0:
                 nums[p0], nums[curr] = nums[curr], nums[p0]
                 p0 += 1
-                curr += 1
             elif nums[curr] == 2:
                 nums[curr], nums[p1] = nums[p1] , nums[curr]
                 p1 -= 1

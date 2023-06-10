b343fa6f93cb4d27c7c620b4a3a4570a50a4b000
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-06-09 21:47:48 -0400

    Time: 183 ms (38.82%), Space: 18 MB (8.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,18 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        p1 = 0
-        p2 = 0
+        # p1 = 0
+        # p2 = 0
+        # n = len(nums)
+        # while p2 < n:
+        #     if not nums[p1]:
+        #         if not nums[p2]:
+        #             p2 += 1
+        #         else:
+        #             nums[p1], nums[p2] = nums[p2], nums[p1]
+        #             p1 += 1
+        #     else:
+        #         p1 += 1
+        #         if p1 > p2:
+        #             p2 = p1
+        
+        slow, fast = 0, 0
         n = len(nums)
-        while p2 < n:
-            if not nums[p1]:
-                if not nums[p2]:
-                    p2 += 1
-                else:
-                    nums[p1], nums[p2] = nums[p2], nums[p1]
-                    p1 += 1
-            else:
-                p1 += 1
-                if p1 > p2:
-                    p2 = p1
+        while fast < n:
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
+            fast += 1
                 

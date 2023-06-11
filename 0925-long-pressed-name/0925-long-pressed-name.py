27de571c115e1feb8a04b2e3141c59764828139d
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-06-10 22:01:11 -0400

    Time: 52 ms (23.84%), Space: 16.3 MB (26.70%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isLongPressedName(self, name: str, typed: str) -> bool:
+        # Two pointers
+        left_1, left_2 = 0, 0
+        while left_1 < len(name) and left_2 < len(typed):
+            if name[left_1] == typed[left_2]:
+                left_1 += 1
+                left_2 += 1
+            else:
+                if left_2 > 0 and typed[left_2] == typed[left_2 - 1]:
+                    left_2 += 1
+                else:
+                    return False
+        while 0 < left_2 < len(typed) and typed[left_2] == typed[left_2 - 1]:
+            left_2 += 1
+
+        if left_1 == len(name) and left_2 == len(typed):
+            return True
+        else:
+            return False
+            

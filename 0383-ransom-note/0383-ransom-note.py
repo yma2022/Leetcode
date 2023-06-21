0f7ec7fb384cd0ebe6ae6f4804729b3fcb60c26b
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-06-21 15:05:44 -0400

    Time: 98 ms (18.42%), Space: 16.6 MB (59.29%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,14 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        
+        hashmap = [0] * 26
+        for c in magazine:
+            hashmap[ord(c) - ord("a")] += 1
+        
+        for c in ransomNote:
+            if hashmap[ord(c) - ord("a")] > 0:
+                hashmap[ord(c) - ord("a")] -= 1
+            else:
+                return False
+        return True
+        

305d7dc2e5e5e5241d8dbbfde223ea3f75518f67
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-06-22 12:58:35 -0400

    Time: 51 ms (45.46%), Space: 16.4 MB (66.54%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        
+        # Two pointers. Another is inside the loop.
+        p = 0
+        while p < len(s):
+            p2 = p + k
+            # Written in this could be more pythonic.
+            s = s[:p] + s[p: p2][::-1] + s[p2:]
+            p = p + 2 * k
+        return s
+        

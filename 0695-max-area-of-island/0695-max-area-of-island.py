1a21e26fe922da321778bbfb791595df30834f69
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-07-16 10:11:42 -0400

    Time: 157 ms (51.34%), Space: 19.8 MB (44.26%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,28 +1,38 @@
 class Solution:
     def __init__(self):
-        self.count = 0
+        self.area = 0
+        self.directions = [
+            [0,1],
+            [1,0],
+            [-1,0],
+            [0,-1]
+        ]
 
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         # DFS
         if not grid: return 0
 
-        m, n = len(grid), len(grid[0])
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        cols, rows = len(grid), len(grid[0])
+        visited = [[False for _ in range(rows)] for _ in range(cols)]
 
         result = 0
-        for i in range(m):
-            for j in range(n):
+        for i in range(cols):
+            for j in range(rows):
                 if not visited[i][j] and grid[i][j] == 1:
-                    self.count = 0
+                    visited[i][j] = True
+                    self.area = 1
                     self.dfs(grid, visited, i, j)
-                    result = max(result, self.count)
+                    result = max(result, self.area)
         return result
         
     def dfs(self, grid, visited, x, y):
-        if visited[x][y] or grid[x][y] == 0:
-            return 
-        visited[x][y] = True
-        self.count += 1
-        for new_x, new_y in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-            if 0 <= new_x < len(grid) and 0 <= new_y < len(grid[0]): 
-                self.dfs(grid, visited, new_x, new_y)
+        for i in range(4):
+            next_x = x + self.directions[i][0]
+            next_y = y + self.directions[i][1]
+            
+            if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]):
+                if not visited[next_x][next_y] and grid[next_x][next_y] == 1:
+                    visited[next_x][next_y] = True
+                    self.area += 1
+                    self.dfs(grid, visited, next_x, next_y)
+            

c7f9211e2f03f5a92f714f7e34718bba1047da19
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-07-28 23:33:41 -0400

    Time: 78 ms (99.40%), Space: 16.3 MB (94.83%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        total_sum = sum(nums)  # 计算nums的总和
+        if abs(target) > total_sum:
+            return 0  # 此时没有方案
+        if (target + total_sum) % 2 == 1:
+            return 0  # 此时没有方案
+        target_sum = (target + total_sum) // 2  # 目标和
+        dp = [0] * (target_sum + 1)  # 创建动态规划数组，初始化为0
+        dp[0] = 1  # 当目标和为0时，只有一种方案，即什么都不选
+        for num in nums:
+            for j in range(target_sum, num - 1, -1):
+                dp[j] += dp[j - num]  # 状态转移方程，累加不同选择方式的数量
+        return dp[target_sum]  # 返回达到目标和的方案数

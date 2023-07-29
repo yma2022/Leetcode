381bb8f19ba183a997d6947307c33a364da3d8c0
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-07-28 20:57:16 -0400

    Time: 708 ms (99.97%), Space: 29.6 MB (97.99%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -0,0 +1,29 @@
+class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.insert(0, 0)
+        heights.append(0)
+        stack = [0]
+        result = 0
+        for i in range(1, len(heights)):
+            # 情况一
+            if heights[i] > heights[stack[-1]]:
+                stack.append(i)
+            # 情况二
+            elif heights[i] == heights[stack[-1]]:
+                stack.pop()
+                stack.append(i)
+            # 情况三
+            else:
+                # 抛出所有较高的柱子
+                while stack and heights[i] < heights[stack[-1]]:
+                    # 栈顶就是中间的柱子，主心骨
+                    mid_index = stack[-1]
+                    stack.pop()
+                    if stack:
+                        left_index = stack[-1]
+                        right_index = i
+                        width = right_index - left_index - 1
+                        height = heights[mid_index]
+                        result = max(result, width * height)
+                stack.append(i)
+        return result

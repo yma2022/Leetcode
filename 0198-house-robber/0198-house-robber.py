5494c64bbd61f3b229dd5cdd3dea7434252dbde3
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-07-31 22:27:47 -0400

    Time: 35 ms (97.28%), Space: 16.4 MB (41.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -10,9 +10,6 @@ class Solution:
         dp[1] = max(nums[0], nums[1])
         
         for i in range(2, n):
-            if dp[i-1] > dp[i-2]:
-                dp[i] = max(dp[i-1], nums[i] + dp[i-2])
-            else:
-                dp[i] = dp[i-2] + nums[i]
+            dp[i] = max(dp[i-1], nums[i] + dp[i-2])
                 
         return dp[n-1]        

c5bbc57ea7f6542e85708e19bc17f3e770d1500c
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-01 14:58:22 -0400

    Time: 54 ms (93.93%), Space: 18.4 MB (83.39%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -6,19 +6,29 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        lookup = {}
-        def dfs(root):
-            if not root: return 0
-            if root in lookup: return lookup[root]
-            val = 0
-            if root.left:
-                val+=dfs(root.left.left) + dfs(root.left.right)
-            if root.right:
-                val+=dfs(root.right.left) + dfs(root.right.right)
-            val=max(val+root.val, dfs(root.left)+dfs(root.right))
-            lookup[root]=val
-            return val
-        return dfs(root)
+        # dp数组（dp table）以及下标的含义：
+        # 1. 下标为 0 记录 **不偷该节点** 所得到的的最大金钱
+        # 2. 下标为 1 记录 **偷该节点** 所得到的的最大金钱
+        dp = self.traversal(root)
+        return max(dp)
+
+    # 要用后序遍历, 因为要通过递归函数的返回值来做下一步计算
+    def traversal(self, node):
+        
+        # 递归终止条件，就是遇到了空节点，那肯定是不偷的
+        if not node:
+            return (0, 0)
+
+        left = self.traversal(node.left)
+        right = self.traversal(node.right)
+
+        # 不偷当前节点, 偷子节点
+        val_0 = max(left[0], left[1]) + max(right[0], right[1])
+
+        # 偷当前节点, 不偷子节点
+        val_1 = node.val + left[0] + right[0]
+
+        return (val_0, val_1)
             
                 
             

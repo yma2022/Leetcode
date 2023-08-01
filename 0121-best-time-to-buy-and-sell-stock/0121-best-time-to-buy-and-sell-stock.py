24dc87f66cc47fbb5d2bdd121e16aa837b937608
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-01 15:01:51 -0400

    Time: 905 ms (91.44%), Space: 27.4 MB (64.49%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,20 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        p_buy = p_sell = 0
-        max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] > prices[p_sell]:
-                p_sell = i
-                max_profit = max(prices[p_sell] - prices[p_buy], max_profit)
-            elif prices[i] < prices[p_buy]:
-                p_buy = i
-                p_sell = i
-            else:
-                pass
-        return max_profit
+        # p_buy = p_sell = 0
+        # max_profit = 0
+        # for i in range(len(prices)):
+        #     if prices[i] > prices[p_sell]:
+        #         p_sell = i
+        #         max_profit = max(prices[p_sell] - prices[p_buy], max_profit)
+        #     elif prices[i] < prices[p_buy]:
+        #         p_buy = i
+        #         p_sell = i
+        #     else:
+        #         pass
+        # return max_profit
+        length = len(prices)
+        dp0, dp1 = -prices[0], 0 #注意这里只维护两个常量，因为dp0的更新不受dp1的影响
+        for i in range(1, length):
+            dp1 = max(dp1, dp0 + prices[i])
+            dp0 = max(dp0, -prices[i])
+        return dp1

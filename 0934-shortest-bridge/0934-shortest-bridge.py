cbfcdcad1d02225394ed33ed4a4028990d098afa
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-05 13:12:56 -0400

    Time: 328 ms (96.38%), Space: 19.3 MB (62.32%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,35 +1,38 @@
 class Solution:
     def shortestBridge(self, A: List[List[int]]) -> int:
-        row,col = len(A),len(A[0])
-        dirs = [(0,0),(-1,0),(0,-1),(1,0),(0,1)]
-        island1 = collections.deque()
-        
+        rows, cols = len(A), len(A[0])
+        dirs = [[0,1],[0,-1],[1,0],[-1,0]]
+        island1 = deque()
         def dfs(x, y):
-            for dx, dy in dirs:
-                nx, ny = x+dx, y+dy
-                if 0<=nx<row and 0<=ny<col and A[nx][ny]==1:
-                    A[nx][ny] = 2
-                    island1.append([nx,ny])
-                    dfs(nx,ny)
-        
-        for i in range(row):
-            for j in range(col):
+            if A[x][y] == 0:
+                return
+            A[x][y] = 2
+            island1.append([x, y])
+            for d in dirs:
+                nx = x + d[0]
+                ny = y + d[1]
+                if 0 <= nx < rows and 0 <= ny < cols:
+                    if A[nx][ny] == 1:                        
+                        dfs(nx, ny)
+        first_x, first_y = 0, 0            
+        for i in range(rows):
+            for j in range(cols):
                 if A[i][j] == 1:
                     first_x, first_y = i, j
                     break
-        dfs(first_x,first_y)            
-        #BFS Expand the island and count step
-        step = 0
+        dfs(first_x, first_y)
+        length = 0
+        print(island1)
         while island1:
             for _ in range(len(island1)):
                 x,y=island1.popleft()
-                for dx,dy in dirs:
-                    nx,ny = x+dx, y+dy
-                    if 0<=nx<row and 0<=ny<col and A[nx][ny]!=2:
+                for d in dirs:
+                    nx,ny = x+d[0], y+d[1]
+                    if 0<=nx<rows and 0<=ny<cols and A[nx][ny]!=2:
                         if A[nx][ny] == 0:
                             A[nx][ny]=2
                             island1.append([nx,ny])
                         elif A[nx][ny] == 1:
-                            return step
-            step+=1
-        # return step
+                            return length
+            length += 1
+        return length          

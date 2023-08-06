acab27fe8384fa70a7558064410d26ca38206d85
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-05 23:59:30 -0400

    Time: 190 ms (85.93%), Space: 19.4 MB (42.07%) - LeetHub

--- 0529-minesweeper/0529-minesweeper.py
+++ 0529-minesweeper/0529-minesweeper.py
@@ -5,9 +5,13 @@ class Solution:
             if board[x][y] == 'M':
                 board[x][y] = 'X'
             elif board[x][y] == 'E':
-                n = sum([board[x + r][y + c] == 'M' for r, c in directions if 0 <= x + r < len(board) and 0 <= y +c < len(board[0])])
+                n = 0
+                for r, c in directions:
+                    if 0 <= x + r < len(board) and 0 <= y +c < len(board[0]):
+                        n += (board[x + r][y + c] == 'M')
                 board[x][y] = str(n) if n else 'B'
-                for r, c in directions * (not n):
-                    self.updateBoard(board, [x + r, y + c])
+                if not n:                    
+                    for r, c in directions:
+                        self.updateBoard(board, [x + r, y + c])
         return board
         

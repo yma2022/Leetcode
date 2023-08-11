ea1deb2b891a7fa885b1015b3596dcffa27640d4
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-10 21:46:53 -0400

    Time: 1473 ms (9.92%), Space: 29.4 MB (20.04%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -1,23 +1,18 @@
 class Solution:
-    def minimizeArrayValue(self, nums: List[int]) -> int:
-        
-        def check_arr(val):
-            carry = 0
-            for i in range(len(nums) - 1, 0, -1):
-                if nums[i] + carry > val:
-                    carry = (nums[i] + carry) - val
-                else:
-                    carry = 0
-            return True if nums[0] + carry <= val else False
-        
+    def minimizeArrayValue(self, nums: List[int]) -> int:        
         left, right = 0, max(nums) + 1
         while left < right:
             mid = (left + right) // 2
-            check = check_arr(mid)
-            if check:
-                right = mid
-            else:
+            carry = 0
+            for i in range(len(nums) - 1, 0, -1):                
+                if nums[i] + carry > mid:
+                    carry = (nums[i] + carry) - mid
+                else:
+                    carry = 0
+            if nums[0] + carry > mid:
                 left = mid + 1
+            else:
+                right = mid
+
         return left
-        
         

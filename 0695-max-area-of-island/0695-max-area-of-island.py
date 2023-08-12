9c8becc127f09de00cc077bf9a75aecd17e312fb
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-12 00:01:52 -0400

    Time: 134 ms (79.98%), Space: 19.4 MB (53.22%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -12,26 +12,27 @@ class Solution:
         # DFS
         if not grid: return 0
 
-        cols, rows = len(grid), len(grid[0])
-        visited = [[False for _ in range(rows)] for _ in range(cols)]
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
         
         def dfs(x, y):
-            for i in range(4):
-                next_x = x + self.directions[i][0]
-                next_y = y + self.directions[i][1]
+            if visited[x][y]:
+                return
+            visited[x][y] = True
+            self.area += 1
+            for d in self.directions:
+                next_x = x + d[0]
+                next_y = y + d[1]
 
                 if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]):
                     if not visited[next_x][next_y] and grid[next_x][next_y] == 1:
-                        visited[next_x][next_y] = True
-                        self.area += 1
                         dfs(next_x, next_y)
         
         result = 0
-        for i in range(cols):
-            for j in range(rows):
+        for i in range(rows):
+            for j in range(cols):
                 if not visited[i][j] and grid[i][j] == 1:
-                    visited[i][j] = True
-                    self.area = 1
+                    self.area = 0
                     dfs(i, j)
                     result = max(result, self.area)
         return result

efe67772222b6709e4f6777251bfb804dab7b7ae
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-11 23:56:45 -0400

    Time: 147 ms (57.03%), Space: 19.4 MB (55.42%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -14,25 +14,27 @@ class Solution:
 
         cols, rows = len(grid), len(grid[0])
         visited = [[False for _ in range(rows)] for _ in range(cols)]
+        
+        def dfs(x, y):
+            for i in range(4):
+                next_x = x + self.directions[i][0]
+                next_y = y + self.directions[i][1]
 
+                if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]):
+                    if not visited[next_x][next_y] and grid[next_x][next_y] == 1:
+                        visited[next_x][next_y] = True
+                        self.area += 1
+                        dfs(next_x, next_y)
+        
         result = 0
         for i in range(cols):
             for j in range(rows):
                 if not visited[i][j] and grid[i][j] == 1:
                     visited[i][j] = True
                     self.area = 1
-                    self.dfs(grid, visited, i, j)
+                    dfs(i, j)
                     result = max(result, self.area)
         return result
         
-    def dfs(self, grid, visited, x, y):
-        for i in range(4):
-            next_x = x + self.directions[i][0]
-            next_y = y + self.directions[i][1]
-            
-            if 0 <= next_x < len(grid) and 0 <= next_y < len(grid[0]):
-                if not visited[next_x][next_y] and grid[next_x][next_y] == 1:
-                    visited[next_x][next_y] = True
-                    self.area += 1
-                    self.dfs(grid, visited, next_x, next_y)
+    
             

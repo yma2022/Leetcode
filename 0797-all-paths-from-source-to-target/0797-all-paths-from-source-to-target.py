0b7bc7dd20cbff6437d21ab25417db41870f27dd
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-15 23:19:37 -0400

    Time: 97 ms (68.40%), Space: 18.4 MB (36.02%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.py
@@ -1,21 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.result = []
-        self.path = [0]
 
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        self.result = []
+        self.path = [0]
+        
         if not graph: return []
-
-        self.dfs(graph, 0)
+        
+        def dfs(root):
+            if root == len(graph) - 1:
+                self.result.append(self.path[:])
+                return
+            for node in graph[root]:
+                self.path.append(node)
+                dfs(node)
+                self.path.pop()
+                
+        dfs(0)
         return self.result
-    
-    def dfs(self, graph, root: int):
-        if root == len(graph) - 1:  # 成功找到一条路径时
-            self.result.append(self.path[:]) 
-            return
-        
-        for node in graph[root]:   # 遍历节点n的所有后序节点
-            self.path.append(node)
-            self.dfs(graph, node)
-            self.path.pop() # 回溯
-        

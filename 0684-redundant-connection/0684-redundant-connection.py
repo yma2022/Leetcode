6d0851e8565fbd54bd3688585f5f33eccd6b2418
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-16 20:46:59 -0400

    Time: 55 ms (93.97%), Space: 16.9 MB (35.68%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,45 +1,28 @@
-class Solution:
-    def __init__(self):
-        """
-        初始化
-        """
-        self.n = 1005
-        self.father = [i for i in range(self.n)]
-
-
-    def find(self, u):
-        """
-        并查集里寻根的过程
-        """
-        if u == self.father[u]:
-            return u
-        self.father[u] = self.find(self.father[u])
-        return self.father[u]
-
-    def join(self, u, v):
-        """
-        将v->u 这条边加入并查集
-        """
-        u = self.find(u)
-        v = self.find(v)
-        if u == v : return
-        self.father[v] = u
-        pass
-
-
-    def same(self, u, v ):
-        """
-        判断 u 和 v是否找到同一个根，本题用不上
-        """
-        u = self.find(u)
-        v = self.find(v)
-        return u == v
+class Solution:    
 
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        self.parent = [i for i in range(n+1)]
+        
+        def find(u):
+            if u == self.parent[u]:
+                return u
+            self.parent[u] = find(self.parent[u])
+            return self.parent[u]
+            
+        def join(u, v):
+            u = find(u)
+            v = find(v)
+            if u == v:
+                return
+            self.parent[v] = u
+            pass
+            
+            
         for i in range(len(edges)):
-            if self.find(edges[i][0]) == self.find(edges[i][1]) :
+            if find(edges[i][0]) == find(edges[i][1]) :
                 return edges[i]
             else :
-                self.join(edges[i][0], edges[i][1])
+                join(edges[i][0], edges[i][1])
         return []
         

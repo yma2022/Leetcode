52658a81d104db5266845989503a6fd4b65a90b9
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-17 23:39:34 -0400

    Time: 272 ms (64.00%), Space: 16.6 MB (49.00%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.py
@@ -1,11 +1,11 @@
 class Solution:
     def findMaxFish(self, grid: List[List[int]]) -> int:
-        seen = set()
+        seen = [[False for _ in range(len(grid[0]))] for _ in range(len(grid))]
         def area(r, c):
             if not (0 <= r < len(grid) and 0 <= c < len(grid[0])
-                    and (r, c) not in seen and grid[r][c]):
+                    and not seen[r][c] and grid[r][c]):
                 return 0
-            seen.add((r, c))
+            seen[r][c] = True
             return (grid[r][c] + area(r+1, c) + area(r-1, c) +
                     area(r, c-1) + area(r, c+1))
 

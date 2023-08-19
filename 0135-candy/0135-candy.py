8ac9fb9d83af6d8d5103bbfc74bc3e98a88c8126
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-19 16:23:52 -0400

    Time: 142 ms (84.44%), Space: 19.3 MB (69.33%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,10 +5,10 @@ class Solution:
         for i in range(1, len(ratings)):
             if ratings[i] > ratings[i-1]:
                 candy[i] = candy[i-1] + 1
-        print(candy)
+        # print(candy)
         for i in range(len(ratings)-2, -1, -1):
             if ratings[i] > ratings[i+1] and candy[i] <= candy[i+1]:
                 candy[i] = candy[i+1] + 1
-        print(candy)
+        # print(candy)
         return sum(candy)
         

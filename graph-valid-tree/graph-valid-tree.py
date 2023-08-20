e36fd77caeed9ff62db2b4be899fa85316db9748
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-20 16:23:53 -0400

    Time: 98 ms, Memory: 17.6 MB - LeetHub

---
+++ graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,33 @@
+class UnionFind:
+    def __init__(self, size):
+        self.parent= [i for i in range(size)]
+        self.rank = [1] * size
+    def find(self, u):
+        if u == self.parent[u]:
+            return u
+        self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+    def union(self, u, v):
+        u = self.find(u)
+        v = self.find(v)
+        if u != v:
+            if self.rank[u] > self.rank[v]:
+                self.parent[v] = u
+            elif self.rank[v] > self.rank[u]:
+                self.parent[u] = v
+            else:
+                self.parent[v] = u
+                self.rank[u] += 1
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        uf = UnionFind(n)
+        n_root = n
+        for e in edges:
+            if uf.find(e[0]) != uf.find(e[1]):
+                uf.union(e[0], e[1])
+                n_root -= 1
+            else:
+                return False
+        return True if n_root == 1 else False
+        

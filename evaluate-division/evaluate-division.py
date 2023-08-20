9d44d1fcb5a9f0e5178f34a0c9b762b1bee89080
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-20 16:49:23 -0400

    Time: 23 ms, Memory: 16.4 MB - LeetHub

---
+++ evaluate-division/evaluate-division.py
@@ -0,0 +1,19 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        def dfs(x, y, visited):
+            if x == y: return 1.0
+            visited.add(x)
+            for n in g[x]:
+                if n in visited: continue
+                visited.add(n)
+                d = dfs(n, y, visited)
+                if d > 0: return d * g[x][n]
+            return -1.0
+
+        g = collections.defaultdict(dict)
+        for (x, y), v in zip(equations, values):      
+            g[x][y] = v
+            g[y][x] = 1.0 / v
+
+        ans = [dfs(x, y, set()) if x in g and y in g else -1 for x, y in queries]
+        return ans

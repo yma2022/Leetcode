120fe63f97a6aa7760f22ca6d18ecd1ea6bd7efc
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-20 23:02:25 -0400

    Time: 44 ms (86.02%), Space: 16.3 MB (75.82%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -1,22 +1,12 @@
 class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         n = len(s)
-        start, end = 0, 1
-        prefix = s[0]
-        curr = 1
-        while curr < n:
-            if prefix[start] != s[curr]:
-                curr = end
-                end += 1                
-                prefix = s[:end]
-                start = 0
-
-            else:
-                start += 1
-                start = start % len(prefix)
-            curr += 1
-            #print(prefix)
-        return len(s) % len(prefix) == 0 and len(prefix) < len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                pattern = s[:i] * (n // i)
+                if s == pattern:
+                    return True
+        return False
                
             
         

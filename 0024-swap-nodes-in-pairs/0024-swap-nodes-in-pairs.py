d6e29e6ed32ebecc91c25d6aa0fb5e9ac7fab9f2
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-23 20:36:26 -0400

    Time: 38 ms (80.23%), Space: 16.3 MB (41.13%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,17 +5,27 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode()
-        dummyHead.next = head
-        curr = dummyHead
-        while curr.next and curr.next.next:
-            dst = curr.next.next.next
-            src = curr.next
-            curr.next = curr.next.next
-            curr.next.next = src
-            src.next = dst
-            curr = curr.next.next
-        return dummyHead.next
+        # dummyHead = ListNode()
+        # dummyHead.next = head
+        # curr = dummyHead
+        # while curr.next and curr.next.next:
+        #     dst = curr.next.next.next
+        #     src = curr.next
+        #     curr.next = curr.next.next
+        #     curr.next.next = src
+        #     src.next = dst
+        #     curr = curr.next.next
+        # return dummyHead.next
+        if not head or not head.next:
+            return head
+        
+        first_node = head
+        second_node = head.next
+        
+        first_node.next = self.swapPairs(second_node.next)
+        second_node.next = first_node
+        
+        return second_node
             
             
         

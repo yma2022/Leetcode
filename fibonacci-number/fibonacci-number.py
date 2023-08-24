0a7d69f191a2405c6512594f35b3eb45d472e93e
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-23 21:26:02 -0400

    Time: 1125 ms, Memory: 16.4 MB - LeetHub

---
+++ fibonacci-number/fibonacci-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # if n == 0:
+        #     return 0
+        # dp = [0] * (2)
+        # dp[0] = 0
+        # dp[1] = 1
+        # for i in range(2,n+1):
+        #     sum = dp[0] + dp[1]
+        #     dp[0] = dp[1]
+        #     dp[1] = sum
+        # return dp[1]
+        d = {0:0,1:1}
+        if n in d:
+            return d[n]
+        if n - 2 in d and n - 1 not in d:
+            return self.fib(n-1) + d[n-2]
+        if n - 1 in d and n - 2 not in d:
+            return self.fib(n-1) + d[n-1]
+        return self.fib(n-1) + self.fib(n-2)

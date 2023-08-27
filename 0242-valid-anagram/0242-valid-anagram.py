c6942c48441367769231b58e7bf8a4d4ffc0d49f
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-27 09:24:03 -0400

    Time: 29 ms (99.97%), Space: 16.7 MB (98.88%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        hashmap = [0] * 26
-        for c in s:
-            hashmap[ord(c) - ord("a")] += 1
-        for c in t:
-            hashmap[ord(c) - ord("a")] -= 1
-        
-        for n in hashmap:
-            if n:
-                return False
-        return True
+        return Counter(s) == Counter(t)
 
         

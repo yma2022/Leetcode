22a13d380563f574bde363ba92134922b9399f20
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-27 09:34:09 -0400

    Time: 95 ms (87.38%), Space: 19.7 MB (69.43%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,12 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = []
         d = collections.defaultdict(list)
         for s in strs:
             key = "".join(sorted(s))
             d[key].append(s)
-        for key in d:
-            res.append(d[key])
+        res = []
+        for k in d:
+            res.append(d[k])
         return res
-                
         

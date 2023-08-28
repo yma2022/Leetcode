8dfa8067f0a9e5648bf32fbfdfde0f53a7195eb5
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-08-27 22:51:40 -0400

    Time: 332 ms (88.43%), Space: 17.9 MB (76.37%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,13 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         left, right = 1, max(piles)
+        def totalT(k, piles):
+            time = 0
+            for p in piles:
+                time += (p // k) if (p % k == 0) else (p // k + 1)
+            return time
         while left < right:
             mid = (left + right) // 2
-            t = 0
-            for p in piles:
-                curr = p // mid + 1 if p % mid != 0 else p // mid
-                t += curr
-            if t > h:
+            time = totalT(mid, piles)
+            if time > h:
                 left = mid + 1
             else:
                 right = mid

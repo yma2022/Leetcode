72b7120840edcfb253dd0fa874fa277b1bea169a
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-01 23:14:09 -0400

    Time: 2510 ms (39.41%), Space: 16.8 MB (30.39%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,26 +1,33 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        def binarySearch(sub, num):
-            left, right = 0, len(sub) - 1            
-            while left < right:
-                mid = (left + right) // 2
-                
-                if sub[mid] < num:
-                    left = mid + 1
-                else:
-                    right = mid
-            return left
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+#         def binarySearch(sub, num):
+#             left, right = 0, len(sub) - 1            
+#             while left < right:
+#                 mid = (left + right) // 2                
+#                 if sub[mid] < num:
+#                     left = mid + 1
+#                 else:
+#                     right = mid
+#             return left
         
-        sub = []
-        sub.append(nums[0])
+#         sub = []
+#         sub.append(nums[0])
         
-        for num in nums:
-            if num > sub[-1]:
-                sub.append(num)
-            else:
-                j = binarySearch(sub, num)
-                sub[j] = num
+#         for num in nums:
+#             if num > sub[-1]:
+#                 sub.append(num)
+#             else:
+#                 j = binarySearch(sub, num)
+#                 sub[j] = num
                 
-        return len(sub)
+#         return len(sub)
+        
+        
     
         

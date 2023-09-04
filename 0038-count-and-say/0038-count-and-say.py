93a09731e01a31606bad415dae637ae1ff536a86
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-04 14:10:21 -0400

    Time: 45 ms (84.03%), Space: 16.2 MB (91.11%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -16,19 +16,4 @@ class Solution:
         if count != 0:
             ans += str(count) + str(cur)
         return ans
-
-        #     if(n==1):
-        #     return("1")
-        # x=self.countAndSay(n-1)
-        # i=0
-        # s=""
-        # while(i<len(x)):
-        #     ch=x[i]
-        #     ns=0
-        #     while(i<len(x) and x[i]==ch):
-        #         ns+=1
-        #         i+=1
-        #     s+=str(ns)
-        #     s+=ch
-        # return(s)
         

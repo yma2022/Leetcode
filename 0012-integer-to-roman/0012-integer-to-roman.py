56bc41f6cfe6079c0840d500d05bd74063e05b09
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-05 12:38:44 -0400

    Time: 59 ms (44.28%), Space: 16.4 MB (15.10%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,36 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        stack = []
+        base = 1
+        num = str(num)
+        for i in range(len(num) - 1, -1, -1):
+            if num[i] != 0:
+                if num[i] == "4":
+                    stack.append((5, base))
+                    stack.append((1, base))
+                elif num[i] == "9":
+                    stack.append((1, base*10))
+                    stack.append((1, base))
+                else:
+                    stack.append((int(num[i]), base))
+            base *= 10
+            
+        d = {1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500: "D", 1000: "M"}
+        
+        ans =""
+        
+        while stack:
+            ele = stack.pop()
+            if ele[0] == 5 or ele[0] == 1:
+                ans += d[ele[0] * ele[1]]
+            else:
+                k = ele[0]
+                while k > 0:
+                    if k > 5:
+                        ans += d[5 * ele[1]]
+                        k -= 5
+                    else:
+                        ans += d[ele[1]]
+                        k -= 1
+        return ans
+        

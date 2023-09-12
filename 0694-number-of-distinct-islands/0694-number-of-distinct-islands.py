95a259ed1387faef1a86e12e15188025e3509492
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-11 23:28:08 -0400

    Time: 209 ms (33.00%), Space: 20.2 MB (30.72%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+
+        # Do a DFS to find all cells in the current island.
+        def dfs(row, col, direction):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]):
+                return
+            if (row, col) in seen or not grid[row][col]:
+                return
+            seen.add((row, col))
+            path_signature.append(direction)
+            dfs(row + 1, col, "D")
+            dfs(row - 1, col, "U")
+            dfs(row, col + 1, "R")
+            dfs(row, col - 1, "L")
+            path_signature.append("0")
+        
+        # Repeatedly start DFS's as long as there are islands remaining.
+        seen = set()
+        unique_islands = set()
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                path_signature = []
+                dfs(row, col, "0")
+                if path_signature:
+                    #print(path_signature)
+                    unique_islands.add(tuple(path_signature))
+        #print(unique_islands)
+        return len(unique_islands)

6ca2d5d96ea05db07f388bf33448c6cefd4d7a75
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-16 22:24:17 -0400

    Time: 154 ms (81.25%), Space: 16.8 MB (98.75%) - LeetHub

--- 1215-stepping-numbers/1215-stepping-numbers.py
+++ 1215-stepping-numbers/1215-stepping-numbers.py
@@ -10,13 +10,16 @@ class Solution:
             res.append(0)
             
         while q:
+            # print(q)
             p = q.popleft()
             if p < high:
                 last = p % 10
                 if last > 0:
-                    q.append(p * 10 + last - 1)
+                    if p * 10 + last - 1 <= high:
+                        q.append(p * 10 + last - 1)
                 if last < 9:
-                    q.append(p * 10 + last + 1)
+                    if p * 10 + last + 1 <= high:
+                        q.append(p * 10 + last + 1)
             if p >= low and p <= high:
                 res.append(p)
         return res

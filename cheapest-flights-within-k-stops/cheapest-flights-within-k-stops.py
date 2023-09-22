e36726d38a1df16ecd98ab088e12eb84471b8ad8
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-21 20:46:08 -0400

    Time: 99 ms, Memory: 17.5 MB - LeetHub

---
+++ cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        seen = set()
+        graph = collections.defaultdict(list)
+        for f in flights:
+            graph[f[0]].append([f[1], f[2]])
+            
+        q = deque()
+        q.append([src, 0])
+        stops = 0
+        dist = [float('inf') for _ in range(n)]
+        while q and stops <= k:
+            length = len(q)
+            for _ in range(length):                
+                node, d = q.popleft()
+                for nei, price in graph[node]:
+                    if price + d < dist[nei]:
+                        dist[nei] = price + d
+                        q.append([nei, dist[nei]])
+            stops += 1
+            
+        return -1 if dist[dst] == float('inf') else dist[dst]
+                

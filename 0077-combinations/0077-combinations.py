e0de965240fe9afec385508940b1e3d420ca7a9c
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-25 22:54:23 -0400

    Time: 983 ms (7.67%), Space: 64.9 MB (17.14%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,14 +1,12 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []  # 存放结果集
-        self.backtracking(n, k, 1, [], result)
-        return result
-    def backtracking(self, n, k, startIndex, path, result):
-        if len(path) == k:
-            result.append(path[:])
-            return
-        for i in range(startIndex, n + 1):  # 需要优化的地方
-            path.append(i)  # 处理节点
-            self.backtracking(n, k, i + 1, path, result)
-            path.pop()  # 回溯，撤销处理的节点
-        
+        def backtrack(n, k, startIndex, path):
+            if len(path) == k:
+                res.append(path[:])
+            for i in range(startIndex, n + 1):
+                path.append(i)
+                backtrack(n, k, i + 1, path)
+                path.pop()
+        res = []
+        backtrack(n, k, 1, [])
+        return res

cb3681a65e281aa890e33246f307dc2a5b8328ac
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-29 22:53:47 -0400

    Time: 373 ms (87.26%), Space: 36.6 MB (39.53%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,25 +1,18 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         n = len(nums)
-        if n < 3:
-            return False
-        min_array = [-1] * n
-        min_array[0] = nums[0]
-        for i in range(1, n):
-            min_array[i] = min(min_array[i - 1], nums[i])
-        k = n   
-        for j in range(n - 1, -1, -1):
-            if nums[j] <= min_array[j]:
-                continue
-            while k < n and nums[k] <= min_array[j]:
-                k += 1
-            if k < n and nums[k] < nums[j]:
+        s3 = float('-inf')
+        stack = []
+        for i in range(n - 1, -1, -1):
+            if nums[i] < s3:
                 return True
-            k -= 1
-            nums[k] = nums[j]
-            # print(nums)
-        
+            else:
+                while stack and nums[i] > stack[-1]:
+                    s3 = stack[-1]
+                    stack.pop()
+            stack.append(nums[i])
         return False
+
             
 #         min_point_after_last_peak = 0
 #         intervals = []

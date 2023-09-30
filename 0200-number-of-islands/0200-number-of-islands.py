e09a2dccd82b9227fc7ea0d0249acdecd3804ec7
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-09-30 15:46:49 -0400

    Time: 300 ms (49.34%), Space: 24.5 MB (19.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,53 +1,23 @@
-class Solution:                    
-#     def numIslands(self, grid: List[List[str]]) -> int:
-#         rows = len(grid)
-#         cols = len(grid[0])
-#         visited = [[False for _ in range(cols)] for _ in range(rows)]
-        
-#         count = 0
-#         dirs = [[0,1],[0,-1],[1,0],[-1,0]]
-        
-#         def dfs(x,y):
-#             for d in dirs:
-#                 nx = x + d[0]
-#                 ny = y + d[1]
-#                 if 0 <= nx < rows and 0 <= ny < cols:
-#                     if not visited[nx][ny] and grid[nx][ny] == "1":
-#                         visited[nx][ny] = True
-#                         dfs(nx, ny)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(x, y):
+            nonlocal seen
+            for dx, dy in [[0,1], [0,-1], [1,0], [-1,0]]:
+                nx, ny = x + dx, y + dy
+                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
+                    if grid[nx][ny] == "1" and (nx, ny) not in seen:
+                        seen.add((nx, ny))
+                        dfs(nx, ny)
+        seen = set()
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1" and (i, j) not in seen:
+                    seen.add((i, j))
+                    dfs(i, j)
+                    count += 1
+                    
+        return count
+                
                         
-#         for i in range(rows):
-#             for j in range(cols):
-#                 if not visited[i][j] and grid[i][j] == "1":
-#                     dfs(i, j)
-#                     count += 1
-#         return count
-        def numIslands(self, grid: List[List[str]]) -> int:
-            if len(grid) == 0: return 0
-            row = len(grid); col = len(grid[0])
-            self.count = sum(grid[i][j]=='1' for i in range(row) for j in range(col))
-            parent = [i for i in range(row*col)]
-
-            def find(x):
-                if parent[x]!= x:
-                    return find(parent[x])
-                return parent[x]
-
-            def union(x,y):
-                xroot, yroot = find(x),find(y)
-                if xroot == yroot: return 
-                parent[xroot] = yroot
-                self.count -= 1
-
-
-
-            for i in range(row):
-                for j in range(col):
-                    if grid[i][j] == '0':
-                        continue
-                    index = i*col + j
-                    if j < col-1 and grid[i][j+1] == '1':
-                        union(index, index+1)
-                    if i < row-1 and grid[i+1][j] == '1':
-                        union(index, index+col)
-            return self.count
+        

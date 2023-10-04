8468784b5031dab8561a87c1100b68a9a86b2668
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-03 23:55:00 -0400

    Time: 42 ms (28.68%), Space: 16.1 MB (93.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,21 +1,18 @@
 class Solution:
-    def __init__(self):
-        self.mapping = {2:"abc", 3:"def", 4:"ghi", 5:"jkl", 6:"mno", 7:"pqrs", 8:"tuv", 9:"wxyz"}
     def letterCombinations(self, digits: str) -> List[str]:
+        d = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+        def backtrack(digits, comb, results):
+            if len(comb) == n:
+                results.append("".join(comb))
+                return
+            for c in d[digits[0]]:
+                comb.append(c)
+                backtrack(digits[1:], comb, results)
+                comb.pop()
         if not digits:
             return []
-        result = []
-        self.backtracking(digits, 0, "", result)
-        return result
-        
-    def backtracking(self, digits, index, string, result):
-        if len(string) == len(digits):
-            result.append(string)
-            return
-        digit = int(digits[index])
-        letters = self.mapping[digit]
-        for i in range(len(letters)):
-            string += letters[i]
-            self.backtracking(digits, index+1, string, result)
-            string = string[:-1]
-        
+        results = []
+        n = len(digits)
+        backtrack(digits, [], results)
+        return results
+                

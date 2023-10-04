f9654c83fecbb42caf58a4d7d5df40073748b31d
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-03 23:55:48 -0400

    Time: 34 ms (82.17%), Space: 16.4 MB (41.50%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -3,16 +3,16 @@ class Solution:
         d = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
         def backtrack(digits, comb, results):
             if len(comb) == n:
-                results.append("".join(comb))
+                results.append(comb)
                 return
             for c in d[digits[0]]:
-                comb.append(c)
+                comb += c
                 backtrack(digits[1:], comb, results)
-                comb.pop()
+                comb = comb[:-1]
         if not digits:
             return []
         results = []
         n = len(digits)
-        backtrack(digits, [], results)
+        backtrack(digits, "", results)
         return results
                 

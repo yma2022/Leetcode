50dffada7857180f5b625456a82d58a35c165ec0
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 18:33:37 -0400

    Time: 152 ms (66.18%), Space: 24.7 MB (37.03%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> umap; //key:a+b的数值，value:a+b数值出现的次数
+        // 遍历大A和大B数组，统计两个数组元素之和，和出现的次数，放到map中
+        for (int a : nums1) {
+            for (int b : nums2) {
+                umap[a + b]++;
+            }
+        }
+        int count = 0; // 统计a+b+c+d = 0 出现的次数
+        // 在遍历大C和大D数组，找到如果 0-(c+d) 在map中出现过的话，就把map中key对应的value也就是出现次数统计出来。
+        for (int c : nums3) {
+            for (int d : nums4) {
+                if (umap.find(0 - (c + d)) != umap.end()) {
+                    count += umap[0 - (c + d)];
+                }
+            }
+        }
+        return count;
+    }
+};

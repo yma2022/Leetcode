6c7a1f01cbd943689f7e05034c5014b95b563bba
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 16:49:53 -0400

    Time: 8 ms (78.24%), Space: 11.4 MB (14.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         std::unordered_map <int, int> map;
         for (int i = 0; i < nums.size(); i++){
-            auto iter = map.find(target - nums[i]);
-            if (iter != map.end()){
-                return {iter->second, i};
-            }
-            map.insert(pair<int, int>(nums[i], i));
+            if(map.find(target - nums[i]) == map.end())
+                map[nums[i]] = i;
+            else
+                return {map[target - nums[i]], i};
         }
         return {};
     }

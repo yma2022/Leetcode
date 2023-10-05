bfb9e3a3941aeb320d419066006398cda5513534
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 18:42:06 -0400

    Time: 3 ms (99.93%), Space: 9.1 MB (29.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         int record[26] = {0};
-        for (int i = 0; i < magazine.size(); i++){
-            record[magazine[i] - 'a']++;
+        for (char ch: magazine){
+            record[ch - 'a']++;
         }
-        for (int i = 0; i < ransomNote.size(); i++){
-            record[ransomNote[i] - 'a']--;
+        for (char ch: ransomNote){
+            record[ch - 'a']--;
         }
         for (int i = 0; i < 26; i++){
             if (record[i] < 0){

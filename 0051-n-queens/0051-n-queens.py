71ca1aeaf0c66bac86befd72131b2a0e179b749d
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 14:35:24 -0400

    Time: 91 ms (29.53%), Space: 16.8 MB (37.37%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,45 +1,42 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        # Making use of a helper function to get the
-        # solutions in the correct output format
-        def create_board(state):
-            board = []
-            for row in state:
-                board.append("".join(row))
-            return board
-        
-        def backtrack(row, diagonals, anti_diagonals, cols, state):
-            # Base case - N queens have been placed
-            if row == n:
-                ans.append(create_board(state))
-                return
+        result = []  # 存储最终结果的二维字符串数组
 
-            for col in range(n):
-                curr_diagonal = row - col
-                curr_anti_diagonal = row + col
-                # If the queen is not placeable
-                if (col in cols 
-                      or curr_diagonal in diagonals 
-                      or curr_anti_diagonal in anti_diagonals):
-                    continue
+        chessboard = ['.' * n for _ in range(n)]  # 初始化棋盘
+        self.backtracking(n, 0, chessboard, result)  # 回溯求解
+        return [[''.join(row) for row in solution] for solution in result]  # 返回结果集
 
-                # "Add" the queen to the board
-                cols.add(col)
-                diagonals.add(curr_diagonal)
-                anti_diagonals.add(curr_anti_diagonal)
-                state[row][col] = "Q"
+    def backtracking(self, n: int, row: int, chessboard: List[str], result: List[List[str]]) -> None:
+        if row == n:
+            result.append(chessboard[:])  # 棋盘填满，将当前解加入结果集
+            return
 
-                # Move on to the next row with the updated board state
-                backtrack(row + 1, diagonals, anti_diagonals, cols, state)
+        for col in range(n):
+            if self.isValid(row, col, chessboard):
+                chessboard[row] = chessboard[row][:col] + 'Q' + chessboard[row][col+1:]  # 放置皇后
+                self.backtracking(n, row + 1, chessboard, result)  # 递归到下一行
+                chessboard[row] = chessboard[row][:col] + '.' + chessboard[row][col+1:]  # 回溯，撤销当前位置的皇后
 
-                # "Remove" the queen from the board since we have already
-                # explored all valid paths using the above function call
-                cols.remove(col)
-                diagonals.remove(curr_diagonal)
-                anti_diagonals.remove(curr_anti_diagonal)
-                state[row][col] = "."
+    def isValid(self, row: int, col: int, chessboard: List[str]) -> bool:
+        # 检查列
+        for i in range(row):
+            if chessboard[i][col] == 'Q':
+                return False  # 当前列已经存在皇后，不合法
 
-        ans = []
-        empty_board = [["."] * n for _ in range(n)]
-        backtrack(0, set(), set(), set(), empty_board)
-        return ans
+        # 检查 45 度角是否有皇后
+        i, j = row - 1, col - 1
+        while i >= 0 and j >= 0:
+            if chessboard[i][j] == 'Q':
+                return False  # 左上方向已经存在皇后，不合法
+            i -= 1
+            j -= 1
+
+        # 检查 135 度角是否有皇后
+        i, j = row - 1, col + 1
+        while i >= 0 and j < len(chessboard):
+            if chessboard[i][j] == 'Q':
+                return False  # 右上方向已经存在皇后，不合法
+            i -= 1
+            j += 1
+
+        return True  # 当前位置合法

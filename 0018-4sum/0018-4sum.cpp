c5605b63da9adb6da9a025f065f2f7489d71b9c2
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 20:39:46 -0400

    Time: 85 ms (36.57%), Space: 13.6 MB (40.16%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        if (nums.size() < 4){
+            return result;
+        }
+        for (int i = 0; i < nums.size() - 3; i++){
+            if (i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            for (int j = i+1; j < nums.size() - 2; j++){
+                if (j > i+1 && nums[j] == nums[j - 1]){
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.size() - 1;
+                while (left < right){
+                    if ((long) nums[i] + nums[j] + nums[left] + nums[right] > target) right--;
+                    else if ((long) nums[i] + nums[j] + nums[left] + nums[right] < target) left++;
+                    else {
+                        result.push_back(vector<int>{nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        left++;
+                        right--;
+                    }
+                }
+                
+            }
+        }
+        return result;
+    }
+};

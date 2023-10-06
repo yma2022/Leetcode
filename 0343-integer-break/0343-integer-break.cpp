bf7bc5297806c41470b4d39eeca0b33a3dcb1fad
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 23:36:50 -0400

    Time: 0 ms (100.00%), Space: 6.5 MB (29.03%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < n + 1; i++){
+            for (int j = 2; j < i; j++){
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i-j]));
+            }
+        }
+        return dp[n];
+    }
+};

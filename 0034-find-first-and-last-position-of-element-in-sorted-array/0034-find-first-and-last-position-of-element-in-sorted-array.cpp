82719635ed9b99e5165bdfef05e48d106e7ee2a5
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 21:18:20 -0400

    Time: 3 ms (92.02%), Space: 14 MB (22.29%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int leftBorder = getLeftBorder(nums, target);
-        int rightBorder = getRightBorder(nums, target);
-        // 情况一
-        if (leftBorder == -2 || rightBorder == -2) return {-1, -1};
-        // 情况三
-        if (rightBorder - leftBorder > 1) return {leftBorder + 1, rightBorder - 1};
-        // 情况二
-        return {-1, -1};
-    }
-private:
-     int getRightBorder(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
-        while (left <= right) {
-            int middle = left + ((right - left) / 2);
-            if (nums[middle] > target) {
-                right = middle - 1;
-            } else { // 寻找右边界，nums[middle] == target的时候更新left
-                left = middle + 1;
-                rightBorder = left;
-            }
+        if (nums.size() == 0)
+            return {-1,-1};
+        
+        int left = 0, right = nums.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        return rightBorder;
-    }
-    int getLeftBorder(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
-        int leftBorder = -2; // 记录一下leftBorder没有被赋值的情况
-        while (left <= right) {
-            int middle = left + ((right - left) / 2);
-            if (nums[middle] >= target) { // 寻找左边界，nums[middle] == target的时候更新right
-                right = middle - 1;
-                leftBorder = right;
-            } else {
-                left = middle + 1;
-            }
+        int end = left - 1;
+        if (end == -1 || nums[end] != target)
+            return {-1,-1};
+        
+        left = 0; right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid;
         }
-        return leftBorder;
+        return {left, end};
     }
 };

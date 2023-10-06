d9c4c6aa91fa7a868ad4d589f987c51139a2c544
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 21:44:14 -0400

    Time: 0 ms (100.00%), Space: 6.6 MB (11.25%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long r = x;
-        while (r*r > x)
-            r = (r + x/r) / 2;
-        return r;       
+        if (x < 2) return x;
+        int l = 1;
+        int r = x / 2;
+
+        while (l <= r) {
+            int m = r - (r - l) / 2;
+            if (m > x / m) r = m - 1;
+            else if (m < x / m) l = m + 1;
+            else return m;
+        }
+        return r;      
     }
 };

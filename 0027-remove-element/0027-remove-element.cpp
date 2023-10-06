7d3240aaaf4684f26ea6d46910836049d33b3ed1
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-05 22:12:22 -0400

    Time: 0 ms (100.00%), Space: 9 MB (24.77%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right){
+            while (left <= right && nums[right] == val) --right;
+            while (left <= right && nums[left] != val) ++left;
+            if (left < right){
+                nums[left] = nums[right];
+                left++;
+                right--;
+            }            
+        }
+        return left;
+    }
+};

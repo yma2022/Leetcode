bcc8705c40c197ef563e94bbe54d453e34aaaf41
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-06 21:04:25 -0400

    Time: 4 ms (30.83%), Space: 9.1 MB (12.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -5,7 +5,7 @@ public:
         int right = nums.size() - 1;
         
         while (left <= right){
-            while (left <= right && nums[right] == val) --right;
+            while (left <= right && nums[right] == val) right--;
             while (left <= right && nums[left] != val) left++;
             if (left < right){
                 nums[left] = nums[right];

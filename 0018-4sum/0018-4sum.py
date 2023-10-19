465ceabd725a38b277555b89a1db293b79745ff5
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-19 17:03:22 -0400

    Time: 1309 ms (15.26%), Space: 16.3 MB (52.92%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,28 +1,31 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        if not nums:
+            return []
         n = len(nums)
         nums.sort()
-        ans = []
+        res = []
         for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             for j in range(i+1, n):
                 if j > i + 1 and nums[j] == nums[j - 1]:
                     continue
-                left = j + 1
-                right = n - 1
-                while left < right:
-                    while left < right and left > j + 1 and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and right < n - 1 and nums[right + 1] == nums[right]:
-                        right -= 1
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
-                    if left < right and total == target:
-                        ans.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
+                l, r = j + 1, n - 1
+                while l < r:
+                    while l < r and l > j + 1 and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and r < n - 1 and nums[r] == nums[r + 1]:
+                        r -= 1
+                    total = nums[i] + nums[j] + nums[l] + nums[r]
+                    if l < r and total == target:
+                        res.append([nums[i], nums[j], nums[l], nums[r]])
+                        l += 1
+                        r -= 1
                     elif total > target:
-                        right -= 1
+                        r -= 1
                     else:
-                        left += 1
-        return ans
+                        l += 1
+        return res
+                    
+        

f15b3bd6e970ed93afae0ee541a1b71b3de497b1
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-19 21:49:47 -0400

    Time: 46 ms (72.63%), Space: 16.5 MB (24.59%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,18 +2,19 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         if n == 1:
             return "1"
-        prev = self.countAndSay(n - 1)
-        ans = ""
-        cur = prev[0]
+        
+        digits = self.countAndSay(n-1)
+        print(digits)
+        res = ""
         count = 1
-        for i in range(1, len(prev)):
-            if prev[i] != cur:
-                ans += str(count) + str(cur)
-                cur = prev[i]
+        curr = digits[0]
+        for c in digits[1:]:
+            if c != curr:
+                res += str(count) + str(curr)
+                curr = c
                 count = 1
             else:
-                count +=1
-        if count != 0:
-            ans += str(count) + str(cur)
-        return ans
-        
+                count += 1
+        if count and curr:
+            res += str(count) + str(curr)
+        return res

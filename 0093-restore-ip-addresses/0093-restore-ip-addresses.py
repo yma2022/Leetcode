81c74b05494dd4232e678c66dc2e154f24efc962
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-21 19:03:43 -0400

    Time: 79 ms (5.12%), Space: 16.4 MB (40.24%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,26 +1,20 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        def backtrack(curr, addr, res):
-            if "".join(addr) == s and len(addr) == 4:
-                res.append(".".join(addr))
+        def backtrack(curr, path, results):
+            if "".join(path[:]) == s and len(path) == 4:
+                results.append(".".join(path[:]))
                 return
+            
             for i in range(curr, len(s)):
-                if isValid(curr, i):
-                    addr.append(s[curr:i+1])
-                    backtrack(i+1, addr, res)
-                    addr.pop()
-                
-                
-        def isValid(start, end):
-            if s[start:end+1].isdigit():
-                if start != end and s[start] == "0":
-                    return False
-                if 0 <= int(s[start:end+1]) <= 255:
-                    return True
-            else:
-                return False
-        
-        results = []
-        backtrack(0, [], results)
-        return results
+                if not s[curr:i+1] or int(s[curr:i+1]) > 255:
+                    continue
+                if s[curr:i+1][0] == "0" and len(s[curr:i+1])>1:
+                    continue
+                path.append(s[curr:i+1])
+                backtrack(i+1, path, results)
+                path.pop()           
+            
+        res = []
+        backtrack(0, [], res)
+        return res
         

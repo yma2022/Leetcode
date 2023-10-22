9e1e943e771570728d28b5f2a9e383b1ee9eb56e
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-21 20:45:21 -0400

    Time: 60 ms (35.74%), Space: 17.2 MB (86.29%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -4,30 +4,27 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def allPossibleBST(self, start, end, memo):
-        res = []
-        if start > end:
-            res.append(None)
-            return res
-        if (start, end) in memo:
-            return memo[(start, end)]
-        
-        # Iterate through all values from start to end to construct left and right subtree recursively.
-        for i in range(start, end + 1):
-            leftSubTrees = self.allPossibleBST(start, i - 1, memo)
-            rightSubTrees = self.allPossibleBST(i + 1, end, memo)
-            
-            # Loop through all left and right subtrees and connect them to ith root.
-            for left in leftSubTrees:
-                for right in rightSubTrees:
-                    root = TreeNode(i, left, right)
-                    res.append(root)
-        
-        memo[(start, end)] = res
-        return res
-    
+class Solution:    
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
+        
+        def allPossibleBST(start, end, memo):
+            res = []
+            if start > end:
+                res.append(None)
+                return res
+            if (start, end) in memo:
+                return memo[(start, end)]
+            
+            for i in range(start, end+1):
+                leftSub = allPossibleBST(start, i-1, memo)
+                rightSub = allPossibleBST(i+1, end, memo)
+                
+                for l in leftSub:
+                    for r in rightSub:
+                        root = TreeNode(i, l, r)
+                        res.append(root)
+            memo[(start, end)] = res
+            return res
         memo = {}
-        return self.allPossibleBST(1, n, memo)
+        return allPossibleBST(1, n, memo)
         

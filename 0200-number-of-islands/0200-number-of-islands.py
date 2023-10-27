3d680fe200681c33d1aedad02628b93b195f4138
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-27 00:38:19 -0400

    Time: 317 ms (35.16%), Space: 24.5 MB (19.90%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,22 +2,23 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(x, y):
             nonlocal seen
-            for dx, dy in [[0,1], [0,-1], [1,0], [-1,0]]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < len(grid) and 0 <= ny < len(grid[0]):
-                    if grid[nx][ny] == "1" and (nx, ny) not in seen:
-                        seen.add((nx, ny))
-                        dfs(nx, ny)
+            seen.add((x,y))
+            for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+                nx, ny = x+dx, y+dy
+                if nx<0 or nx>=len(grid) or ny<0 or ny>=len(grid[0]):
+                    continue
+                if (nx, ny) in seen or grid[nx][ny] == "0":
+                    continue
+                dfs(nx, ny)
+
+            
         seen = set()
-        count = 0
+        res = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1" and (i, j) not in seen:
-                    seen.add((i, j))
-                    dfs(i, j)
-                    count += 1
-                    
-        return count
-                
-                        
+                if grid[i][j] == "0" or (i,j) in seen:
+                    continue
+                dfs(i, j)
+                res += 1
         
+        return res

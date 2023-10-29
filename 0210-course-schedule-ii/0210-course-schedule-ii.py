8761e6a8a110fc8dc19c8c2dfb44a078d9af0420
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-10-28 23:18:05 -0400

    Time: 90 ms (94.47%), Space: 19.3 MB (41.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,21 +1,20 @@
 class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        courses = collections.defaultdict(set)
-        pres = collections.defaultdict(set)
-        res = []
-        for course, pre in prerequisites:
-            courses[course].add(pre)
-            pres[pre].add(course)
-        stack=[n for n in range(numCourses) if not courses[n]]
-        count = 0
-        while stack:
-            no_pre = stack.pop()
-            res.append(no_pre)
-            count+=1
-            for course in pres[no_pre]:
-                courses[course].remove(no_pre)
-                if not courses[course]:
-                    stack.append(course)
-        return res if count==numCourses else []
+    def findOrder(self, N: int, P: List[List[int]]) -> List[int]:
+        G, indegree, ans = defaultdict(list), [0]*N, []
+        for nxt, pre in P:
+            G[pre].append(nxt)
+            indegree[nxt] += 1
         
-        
+        def dfs(cur):
+            ans.append(cur)
+            indegree[cur] = -1
+            for nextCourse in G[cur]:
+                indegree[nextCourse] -= 1
+                if indegree[nextCourse] == 0: 
+                    dfs(nextCourse)            
+        for i in range(N):
+            if indegree[i] == 0:
+                dfs(i)
+
+        return ans if len(ans) == N else []
+                

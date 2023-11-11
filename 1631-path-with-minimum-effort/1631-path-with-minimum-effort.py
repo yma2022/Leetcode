efca52b4308d3e0d70b75700145abd7d40bde720
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-11-11 15:48:41 -0500

    Time: 3659 ms (5.01%), Space: 18.5 MB (41.83%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,25 +1,22 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        row = len(heights)
-        col = len(heights[0])
-        difference_matrix = [[math.inf]*col for _ in range(row)]
-        difference_matrix[0][0] = 0
-        visited = [[False]*col for _ in range(row)]
-        queue = [(0, 0, 0)]  # difference, x, y
-        while queue:
-            difference, x, y = heapq.heappop(queue)
-            visited[x][y] = True
-            for dx, dy in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
-                adjacent_x = x + dx
-                adjacent_y = y + dy
-                if 0 <= adjacent_x < row and 0 <= adjacent_y < col and not visited[
-                        adjacent_x][adjacent_y]:
-                    current_difference = abs(
-                        heights[adjacent_x][adjacent_y]-heights[x][y])
-                    max_difference = max(
-                        current_difference, difference_matrix[x][y])
-                    if difference_matrix[adjacent_x][adjacent_y] > max_difference:
-                        difference_matrix[adjacent_x][adjacent_y] = max_difference
-                        heapq.heappush(
-                            queue, (max_difference, adjacent_x, adjacent_y))
-        return difference_matrix[-1][-1]
+        def isPath(effort: int) -> bool:
+            seen, dq = {(0, 0)}, deque([(0, 0)])
+            while dq:
+                x, y = dq.popleft()
+                if (x, y) == (len(heights) - 1, len(heights[0]) - 1):
+                    return True
+                for r, c in (x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y):
+                    if len(heights) > r >= 0 <= c < len(heights[0]) and abs(heights[r][c] - heights[x][y]) <= effort and (r, c) not in seen:
+                        seen.add((r, c))
+                        dq.append((r, c))
+            return False            
+        
+        lo, hi = 0, 10 ** 6
+        while lo < hi:
+            effort = lo + hi >> 1
+            if isPath(effort):
+                hi = effort
+            else:
+                lo = effort + 1
+        return lo

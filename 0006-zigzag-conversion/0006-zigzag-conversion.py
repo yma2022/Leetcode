f03d9b1e2cc6dc845e7dc866dca679e9fb86b668
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-11-29 23:38:29 -0500

    Time: 66 ms (36.38%), Space: 16.5 MB (41.05%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -2,24 +2,17 @@ class Solution:
     def convert(self, s: str, numRows: int) -> str:
         if numRows == 1:
             return s
-        
-        res = [""] * numRows
-        curr = -1
-        signal = 1
-        
+        res = [[] for _ in range(numRows)]
+        flag = 1
+        currRow = 0
         for i in range(len(s)):
-            if signal == 1:
-                curr += 1
-                res[curr] += s[i]
-                if curr == numRows - 1:
-                    signal = -1
-            elif signal == -1:
-                curr -=1
-                res[curr] += s[i]
-                if curr == 0:
-                    signal = 1
-                    
-        # print(res)    
-        return "".join(res)
+            if i > 0 and i % (numRows - 1) == 0:
+                flag *= -1
+            res[currRow].append(s[i])
+            currRow += flag
             
+        ans = ""
+        for i in range(numRows):
+            ans += "".join(res[i])
+        return ans
         

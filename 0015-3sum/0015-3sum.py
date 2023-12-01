accd36ac1247dfc7cb8b3f6870272ce56a670e38
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-01 14:47:45 -0500

    Time: 3037 ms (16.60%), Space: 20.5 MB (48.91%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,25 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Hashmap
-        # res, dups = set(), set()
-        # seen = {}
-        # for i, val1 in enumerate(nums):
-        #     if val1 not in dups:
-        #         dups.add(val1)
-        #         for j, val2 in enumerate(nums[i+1:]):
-        #             complement = -val1 - val2
-        #             if complement in seen and seen[complement] == i:
-        #                 res.add(tuple(sorted((val1, val2, complement))))
-        #             seen[val2] = i
-        # return res
-        
-        n = len(nums)
+        res = []
         nums.sort()
-        ans = []
-
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
+        n = len(nums)
+        for i in range(n-1):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
-            left = i + 1
-            right = n - 1
-            while left < right:
-                while left < right and left > i + 1 and nums[left] == nums[left - 1]:
-                    left += 1
-                while left < right and right < n - 1 and nums[right + 1] == nums[right]:
-                    right -= 1
-                if left < right and nums[i] + nums[left] + nums[right] == 0:
-                    ans.append([nums[i], nums[left], nums[right]])
-                    left += 1
-                    right -= 1
-                elif nums[i] + nums[left] + nums[right] > 0:
-                    right -= 1
-                else:
-                    left += 1
-        return ans
+            l, r = i+1, n-1
+            while l < r:
+                while i+1 < l < n and nums[l] == nums[l-1]:
+                    l += 1
+                while 0 < r < n-1 and nums[r] == nums[r+1]:
+                    r -= 1
+                if l < r and nums[l] + nums[r] + nums[i] == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                elif l < r and nums[l] + nums[r] + nums[i] > 0:
+                    r -= 1
+                elif l < r and nums[l] + nums[r] + nums[i] < 0:
+                    l += 1
+        return res
+                
+        

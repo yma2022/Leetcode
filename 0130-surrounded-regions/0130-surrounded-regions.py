6259c459922c72ac85ef9a703bd306cf2e09761f
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-04 20:35:27 -0500

    Time: 143 ms (35.37%), Space: 18.2 MB (53.19%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,40 +3,34 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        o_set = set()
-        
+        def dfs(x, y):
+            if board[x][y] != 'O':
+                return
+            board[x][y] = '+'
+            
+            for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nx, ny = x+dx, y+dy
+                if nx<0 or nx>=len(board) or ny<0 or ny>=len(board[0]):
+                    continue
+                if board[nx][ny] != 'O':
+                    continue
+                dfs(nx,ny)
         rows = len(board)
         cols = len(board[0])
-        
         for i in range(rows):
-            for j in range(cols):
-                if board[i][j] != "O":
-                    continue
-                o_set.add((i, j))
-                board[i][j] = "X"
-                    
-        def dfs(x, y):
-            nonlocal seen
-            if (x, y) in seen:
-                return
-            seen.add((x, y))
-            board[x][y] = "O"
-            for dx, dy in [[1,0], [-1,0], [0,1], [0,-1]]:
-                nx, ny = x+dx, y+dy
-                if nx<0 or nx>=rows or ny<0 or ny>=cols:
-                    continue
-                if (nx, ny) not in o_set or (nx, ny) in seen:
-                    continue
-                dfs(nx, ny)
-        seen = set()
-        
-        for i in range(rows):
-            if (i ,0) in o_set:
+            if board[i][0] == 'O':
                 dfs(i, 0)
-            if (i, cols-1) in o_set:
+            if board[i][cols-1] == 'O':
                 dfs(i, cols-1)
         for i in range(cols):
-            if (0, i) in o_set:
+            if board[0][i] == 'O':
                 dfs(0, i)
-            if (rows-1, i) in o_set:
+            if board[rows-1][i] == 'O':
                 dfs(rows-1, i)
+                
+        for i in range(rows):
+            for j in range(cols):
+                if board[i][j] == '+':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'

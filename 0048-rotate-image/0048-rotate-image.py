841e96a90e89585b1da2f25aca3adf9848c69123
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-12 14:41:06 -0500

    Time: 38 ms (81.27%), Space: 16.2 MB (70.00%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,10 +3,8 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n = len(matrix[0])
-        for i in range(n // 2):
-            for j in range((n + 1) // 2):
-                matrix[i][j], matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1] = matrix[n - j - 1][i], matrix[n - i - 1][n - j - 1], matrix[j][n - i - 1], matrix[i][j]
-
-        
-        
+        m = len(matrix)
+        n = m - 1
+        for i in range(m//2):
+            for j in range((m+1)//2):
+                matrix[i][j], matrix[n - j][i],matrix[n-i][n-j],matrix[j][n-i] = matrix[n - j][i],matrix[n-i][n-j],matrix[j][n-i], matrix[i][j]

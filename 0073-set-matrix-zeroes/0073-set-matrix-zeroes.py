cd4121792b0dbf35bf690f5528e3188ca65c7652
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-12 14:49:21 -0500

    Time: 108 ms (93.69%), Space: 17.3 MB (39.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,22 +3,22 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = set()
-        cols = set()
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        
+        rowSet = set()
+        colSet = set()
+        
+        for i in range(rows):
+            for j in range(cols):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+                    rowSet.add(i)
+                    colSet.add(j)
                     
-        for r in rows:
-            for i in range(len(matrix[0])):
-                if matrix[r][i] == 0:
-                    continue
-                matrix[r][i] = 0
-        for c in cols:
-            for i in range(len(matrix)):
-                if matrix[i][c] == 0:
-                    continue
-                matrix[i][c] = 0
+        for row in rowSet:
+            matrix[row] = [0] * cols
+            
+        for col in colSet:
+            for i in range(rows):
+                matrix[i][col] = 0
         

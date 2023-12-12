0caf4ebf995bfbe7e6a78a2137db24a55fdb3328
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-11 23:36:33 -0500

    Time: 62 ms (87.23%), Space: 18.2 MB (17.30%) - LeetHub

---
+++ 0120-triangle/0120-triangle.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[-1] * n for _ in range(n)]
+        dp[n - 1] = triangle[n - 1]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                lower_left = triangle[i][j] + dp[i + 1][j]
+                lower_right = triangle[i][j] + dp[i + 1][j + 1]
+                dp[i][j] = min(lower_left, lower_right)
+        print(dp)
+        return dp[0][0]

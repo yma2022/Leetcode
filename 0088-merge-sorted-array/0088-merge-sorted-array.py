b1ffd3f72e012dab325ac954e20b2daedea0c353
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-12 20:44:47 -0500

    Time: 40 ms (77.90%), Space: 16.3 MB (38.49%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,14 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = m - 1
-        p2 = n - 1
-        for p in range(n+m-1, -1, -1):
-            if p2 < 0:
-                break
-            if p1 >= 0 and nums1[p1] > nums2[p2]:
-                nums1[p] = nums1[p1]
-                p1 -= 1
-            else:
-                nums1[p] = nums2[p2]
-                p2 -= 1
+        p = m - 1
+        q = n - 1
+        
+        for i in range(len(nums1) - 1, -1, -1):
+            if p >= 0 and q >= 0:
+                if nums2[q] > nums1[p]:
+                    nums1[i] = nums2[q]
+                    q -= 1
+                else:
+                    nums1[p], nums1[i] = nums1[i], nums1[p]
+                    p -= 1
+            elif p >= 0:
+                nums1[p], nums1[i] = nums1[i], nums1[p]
+                p -= 1
+            elif q >= 0:
+                nums1[i] = nums2[q]
+                q -= 1
+        
+            
+        

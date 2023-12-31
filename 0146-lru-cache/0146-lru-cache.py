50f42d400e0258178542868a6005a9e42608eebe
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-31 14:44:30 -0500

    Time: 1136 ms (10.67%), Space: 78.1 MB (67.12%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,24 +1,23 @@
-from collections import OrderedDict
-class LRUCache(OrderedDict):
+class LRUCache:
 
     def __init__(self, capacity: int):
-        self.capacity = capacity
+        self.dict = {}
+        self.capacity = capacity   
 
     def get(self, key: int) -> int:
-        if key not in self:
+        if key not in self.dict:
             return -1
-        
-        self.move_to_end(key)
-        return self[key]
-        
+        val = self.dict.pop(key)  #Remove it first before inserting it at the end again
+        self.dict[key] = val   
+        return val        
 
     def put(self, key: int, value: int) -> None:
-        if key in self:
-            self.move_to_end(key)
-        self[key] = value
-        
-        if len(self) > self.capacity:
-            self.popitem(last = False)
+        if key in self.dict:    
+            self.dict.pop(key)
+        else:
+            if len(self.dict) == self.capacity:
+                del self.dict[next(iter(self.dict))]         
+        self.dict[key] = value
         
 
 

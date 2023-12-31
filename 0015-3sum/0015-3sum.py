bd904090bb7896ce4ea4c4863694d83d05d96232
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-31 15:33:53 -0500

    Time: 2915 ms (16.55%), Space: 21.1 MB (27.84%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        n = len(nums)
-        for i in range(n-1):
-            if i > 0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i+1, n-1
-            while l < r:
-                while i+1 < l < n and nums[l] == nums[l-1]:
-                    l += 1
-                while 0 < r < n-1 and nums[r] == nums[r+1]:
-                    r -= 1
-                if l < r and nums[l] + nums[r] + nums[i] == 0:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l += 1
-                    r -= 1
-                elif l < r and nums[l] + nums[r] + nums[i] > 0:
-                    r -= 1
-                elif l < r and nums[l] + nums[r] + nums[i] < 0:
-                    l += 1
-        return res
+        triplets = set()
+        for i in range(len(nums) - 2):
+            firstNum = nums[i]
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                secondNum  = nums[j]
+                thirdNum = nums[k]
+
+                potentialSum = firstNum + secondNum + thirdNum 
+                if potentialSum > 0:
+                    k -= 1
+                elif potentialSum < 0:
+                    j += 1
+                else:
+                    triplets.add((firstNum , secondNum ,thirdNum))
+                    j += 1
+                    k -= 1
+        return triplets
+                    
                 
+        
         

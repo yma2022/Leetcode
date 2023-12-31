a9aec62e8387504d95154043c5e0c96a7d742030
yma2022 <99157647+yma2022@users.noreply.github.com>
2023-12-31 15:05:10 -0500

    Time: 283 ms (63.30%), Space: 25.1 MB (20.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,28 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         def dfs(x, y):
-            nonlocal seen
-            if (x, y) in seen:
+            if (x,y) in visited:
                 return
-            seen.add((x, y))
-            for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nx, ny = x+dx, y+dy
-                if (nx, ny) in seen:
+            
+            visited.add((x,y))
+            
+            for dx, dy in [[0,1],[0,-1],[1,0],[-1,0]]:
+                nx, ny = x + dx, y + dy
+                if nx < 0 or nx >= len(grid) or ny < 0 or ny >= len(grid[0]):
                     continue
-                if nx<0 or nx>=len(grid) or ny<0 or ny>=len(grid[0]):
+                if (nx,ny) in visited:
                     continue
-                if grid[nx][ny] == '0':
+                if grid[nx][ny] == "0":
                     continue
                 dfs(nx, ny)
+        
+        visited = set()
         count = 0
-        seen = set()
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == '0':
-                    continue
-                if (i, j) in seen:
-                    continue
-                dfs(i, j)
-                count += 1
+                if grid[i][j] == "1" and (i,j) not in visited:
+                    dfs(i,j)
+                    count += 1
+                    
         return count
-                
         

040f6d4007aa0aa6cf8f981816f83fa6b17fe5fe
yma2022 <99157647+yma2022@users.noreply.github.com>
2024-01-04 15:41:28 -0500

    Time: 39 ms (54.13%), Space: 17.4 MB (12.69%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -2,17 +2,18 @@ class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         hashmap = {"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
         def backtrack(d, path, res):
-            if not d:
+            if len(path) == len(digits):
                 res.append("".join(path[:]))
                 return
-            
             for ch in hashmap[d[0]]:
                 path.append(ch)
                 backtrack(d[1:], path, res)
                 path.pop()
+              
         if not digits:
             return []
-        res = []       
+        res = []
         backtrack(digits, [], res)
+        
         return res
         

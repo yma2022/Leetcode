05483f66bdbc9646d14f5606e214bb32d4571e77
yma2022 <99157647+yma2022@users.noreply.github.com>
2024-01-04 23:52:24 -0500

    Time: 33 ms (87.54%), Space: 17.4 MB (10.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,25 +1,10 @@
 class Solution:
-    # def myPow(self, x: float, n: int) -> float:
-        # if x == 0.0:
-        #     return 0.0
-        # # res = 1
-        # if n < 0:
-        #     x = 1/x
-        #     n = -n
-        # while n:
-        #     if n & 1:
-        #         res *= x
-        #     x *= x
-        #     n //= 2
-        # return res
-    def binaryExp(self, x, n):
+    def myPow(self, x: float, n: int) -> float:
         if n == 0:
             return 1
         if n < 0:
-            return 1.0 / self.binaryExp(x, -n)
-        if n % 2 == 1:
-            return x * self.binaryExp(x * x, (n - 1) // 2)
+            return 1.0 / self.myPow(x, -n)
+        if n % 2 == 0:
+            return self.myPow(x*x, n // 2) 
         else:
-            return self.binaryExp(x * x, n // 2)
-    def myPow(self, x: float, n: int) -> float:
-        return self.binaryExp(x, n)
+            return x*self.myPow(x*x, (n-1) // 2)

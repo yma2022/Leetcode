b0bdaa164e7fc14dea14df0ec31965eab25ab463
yma2022 <99157647+yma2022@users.noreply.github.com>
2024-01-25 21:41:55 -0500

    Time: 548 ms (66.65%), Space: 41.8 MB (74.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0 for _ in range(len(text2))] for _ in range(len(text1))]
-        dp[0][0] = 1 if text1[0] == text2[0] else 0
-        for i in range(1, len(text1)):
-            dp[i][0] = min(1, dp[i-1][0] + 1) if text1[i] == text2[0] else dp[i-1][0]
-            
-        for j in range(len(text2)):
-            dp[0][j] = min(1, dp[0][j-1] + 1) if text1[0] == text2[j] else dp[0][j-1]
-            
-        for i in range(1,len(text1)):
-            for j in range(1,len(text2)):
-                if text1[i] == text2[j]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+        m = len(text1)
+        n = len(text2)
+        
+        dp = [[0]*(n+1) for i in range(m+1)]
+        
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                    
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        # print(dp)            
+                    dp[i][j] = max(dp[i-1][j-1], dp[i][j-1], dp[i-1][j])
+                    
         return dp[-1][-1]
         
